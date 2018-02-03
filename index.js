9f6eb36c82d9aac66747215ed48972f96b1419e4
fffuture <t30t40t50@gmail.com>
2018-02-03 14:53:52 +0200

    create request workflow store request to db vocabulary

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
 require('dotenv').config();
+
 
 let __run__;
 
 class Bot {
   constructor(token = process.env.TOKEN) {
+    this.dictionary = low(new FileSync('dictionary.json')).getState();
+    this.db = low(new FileSync('db.json'));
+
+    if (!this.db.has('posts').value()) {
+      this.db.defaults({ cvs: [] }).write();
+    }
+
     this.bot = new TelegramBot(token, { polling: true });
+
     this.echoCallback = this.echoCallback.bind(this);
     this.messageCallback = this.messageCallback.bind(this);
+    this.createCV = this.createCV.bind(this);
+    this.rejectCV = this.rejectCV.bind(this);
 
 
     this.bot.onText(/\/echo (.+)/, this.echoCallback);
+    this.bot.onText(/\/cancel/, this.rejectCV);
+    this.bot.onText(/\/reject (.+)/, this.rejectCV);
+    this.bot.onText(/\/create/, this.createCV);
+
     this.bot.on('message', this.messageCallback);
 
     this.bot.on('polling_error', (error) => {
-      console.log(error.code);  // => 'EFATAL'
+      console.log(error);  // => 'EFATAL'
     });
   }
 
@@ -26,9 +43,74 @@ class Bot {
   }
 
   messageCallback(msg) {
-    const chatId = msg.chat.id;
-    this.bot.sendMessage(chatId, 'Received your message');
+    const record = this.db.get('cvs').find({ id: msg.chat.id });
+
+    // if stage on start
+    if (!record.value()) {
+      return this.createCV(msg);
+    }
+
+    if (msg.entities && msg.text === '/cancel') return; // kostil
+
+    if (record.value().stage === 'pending') {
+      return this.bot.sendMessage(msg.chat.id, this.dictionary.cancel);
+    }
+
+    if (record.value().stage === 'info' && !msg.photo) {
+      return this.bot.sendMessage( msg.chat.id, this.dictionary.attachPhoto);
+    }
+
+    if (record.value().stage === 'info' && msg.photo) {
+      record.assign({
+        stage: 'pending',
+        photo: msg.photo,
+      }).write();
+      return this.bot.sendMessage(msg.chat.id, this.dictionary.cancel);
+    }
+
+    if (!msg.entities) {
+      return this.bot.sendMessage( msg.chat.id, this.dictionary.hashtag);
+    }
+
+    record.assign({
+        stage: 'info',
+        text: msg.text,
+        hashtag: msg.entities.map(item => msg.text.slice(item.offset, item.offset + item.length)),
+      }).write();
+
+    this.bot.sendMessage( msg.chat.id, this.dictionary.attachPhoto);
   }
+
+  createCV(msg) {
+    const chatId = msg.chat.id;
+    const db = this.db.get('cvs');
+    const record = db.find({ id: msg.chat.id });
+    if (!record.value()) {
+      db.push({
+        id: msg.chat.id,
+        username: `@${msg.from.username}`,
+        stage: 'start',
+      }).write()
+    }
+    this.bot.sendMessage(chatId, this.dictionary.welcome);
+  }
+
+  notifyAdmin() {}
+
+  notifyUser() {}
+
+  approveCV() {}
+
+  rejectCV(msg, match=null) {
+    if (match[1]) return console.log('bb', match)
+    const record = this.db.get('cvs').find({ id: msg.chat.id });
+    record.assign({
+      id: `00000${msg.chat.id}`,
+      stage: 'rejected'
+    }).write();
+    return this.bot.sendMessage(msg.chat.id, this.dictionary.done);
+  }
+
 }
 
 if (!module.parent) {
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 const requestPromise = require('request-promise');
 const { expect } = require('chai');
 require('dotenv').config();
@@ -38,4 +40,42 @@ describe('Existential Bot Test', () => {
       done();
     }).catch(done);
   });
+});
+
+
+describe('Test user\'s workflow', () => {
+  let dictionary = low(new FileSync('dictionary.json')).getState();
+  let db = low(new FileSync('db.json'));
+
+  beforeEach(() => {
+    db.defaults({ cvs: [] }).write();
+  })
+
+  it('should create request', (done) => {
+    request(apis.sendMessage + 'test+initiate').then((resp) => {
+
+
+      expect(resp.ok).to.equal(true);
+      expect(resp.result.message_id).to.be.an('number');
+      expect(resp.result.text).to.equal('test initiate');
+
+      expect(db.get('cvs').find({ id: resp.result.chat.id }).value()).not.to.be.undefined();
+
+      done();
+    }).catch(done);
+  });
+
+  it('should accept main info', (done) => {
+    return done();
+    request(apis.sendMessage + dictionary.welcome).then((resp) => {
+
+      console.log(resp.result);
+      expect(resp.ok).to.equal(true);
+      expect(resp.result.message_id).to.be.an('number');
+      expect(resp.result.text).to.equal(dictionary.attachPhoto);
+
+      done();
+    }).catch(done);
+  });
+
 });
